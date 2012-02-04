405a0df989371dd35b6dfa92350162a482c97854
Minoru KAWAMOTO <minoru-github@k12u.org>
2012-02-05 02:13:10 +0900

    add 3rd

---
+++ 3/sparse.c
@@ -0,0 +1,28 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+char buf1[] = "abcdefghij";
+char buf2[] = "ABCDEFGHIJ";
+
+#define FILE_MODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+
+int
+main(void)
+{
+  int fd;
+  if( (fd = creat("file.hole", FILE_MODE)) < 0)
+    perror("creat error");
+
+  if (write(fd, buf1, 10) != 10)
+    perror("buf1 write error");
+
+  if(lseek(fd, 40, SEEK_SET) == -1)
+    perror("lseek error");
+
+  if (write(fd, buf2, 10) != 10)
+    perror("buf2 write error");
+
+  exit(0);
+}
